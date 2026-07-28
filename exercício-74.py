4ced323f47c0352ac4cd493f787fcb40438c3bf0
Marcos <marcosvieira.vs11@gmail.com>
2026-07-28 11:45:36 -0400

    74 - Números Organizados + Tupla

---
+++ exercício-74.py
@@ -0,0 +1,5 @@
+from random import randint
+aleatorios = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+print(f'Números gerados: {aleatorios}')
+print(f'Maior número: {max(aleatorios)}')
+print(f'Menor número: {min(aleatorios)}')
--- teste.py
+++ teste.py
@@ -1,8 +1,5 @@
-soma = 0
-s_pares = []
-for i in range(0, 6):
-    num = int(input(f'Digite o {i+1}° valor: '))
-    if num % 2 == 0:
-        soma += num
-        s_pares.append(num)
-print(f'A soma dos números {(', '.join(map(str, s_pares)))}, que são pares, é igual a {soma}')
+teste = 7, 10, 9, 1, 4
+print(teste)
+print(type((teste)))
+print(max(teste))
+print(min(teste))
