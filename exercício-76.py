18467de0da24a71d0a2a9cabf2ec67a9d59d85f7
Marcos <marcosvieira.vs11@gmail.com>
2026-07-29 08:07:14 -0400

    76 - Vieira Atacado

---
+++ exercício-76.py
@@ -0,0 +1,6 @@
+produtos_preços = 'Playstation 5', 3799.97, 'Geladeira Frost Free', 2197.83, 'Amor Recíproco', 9999999.99, 'Claude Max', 549.99, 'MacBook Air M1', 2899.89, 'Camiseta I - Corinthians 26/27', 379.99
+print(50*'=')
+print(f'{'Tabela de Preços - Vieira Atacado':^50}')
+print(50*'=')
+for i in range(0, len(produtos_preços), 2):
+    print(f'{produtos_preços[i]:.<35}RS{produtos_preços[i+1]:,.2f}')
