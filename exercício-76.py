20f61406339ae5a00d632d6c0dfc7cbae3a51d2e
Marcos <marcosvieira.vs11@gmail.com>
2026-07-30 11:18:53 -0400

    76 - Vieira Atacado + Explicação

--- exercício-76.py
+++ exercício-76.py
@@ -2,5 +2,7 @@ produtos_preços = 'Playstation 5', 3799.97, 'Geladeira Frost Free', 2197.83, 'A
 print(50*'=')
 print(f'{'Tabela de Preços - Vieira Atacado':^50}')
 print(50*'=')
+# Usando o for e com o passo 2, consigo trazer as duas informações que preciso mostrar na primeira linha.
 for i in range(0, len(produtos_preços), 2):
+    # Mostra o nome do produto e em seguida o preço.
     print(f'{produtos_preços[i]:.<35}RS{produtos_preços[i+1]:,.2f}')
