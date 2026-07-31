03183819ea5649d931878195ad6fd04f949b2e0d
Marcos <marcosvieira.vs11@gmail.com>
2026-07-31 16:23:23 -0400

    74 - Gerador de Números (?) + Explicação

--- exercício-74.py
+++ exercício-74.py
@@ -1,5 +1,9 @@
 from random import randint
+# Uso o randint para gerar 5 números entre 0 a 10.
 aleatorios = (randint(0,10), randint(0,10), randint(0,10), randint(0,10), randint(0,10))
+# Mostrar os números gerados.
 print(f'Números gerados: {aleatorios}')
+# Utilizo a função max para pegar o maior número
 print(f'Maior número: {max(aleatorios)}')
+# Utilizo a função min para pegar o menor número
 print(f'Menor número: {min(aleatorios)}')
