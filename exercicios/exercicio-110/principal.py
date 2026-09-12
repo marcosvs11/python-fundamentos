34656bc29d0b64d24b9cb771b0b94d534682bd83
Marcos <marcosvieira.vs11@gmail.com>
2026-09-12 17:29:32 -0400

    110 - Função Resumo Monetário

---
+++ exercicios/exercicio-110/principal.py
@@ -0,0 +1,6 @@
+import uteis
+
+valor = float(input('Digite um valor: R$'))
+taxaAcrescimo = float(input('Digite uma taxa em porcentagem para acréscimo: '))
+taxaDecrescimo = float(input('Digite outra taxa percentual para decréscimo: '))
+uteis.resumo(valor, taxaAcrescimo, taxaDecrescimo)
